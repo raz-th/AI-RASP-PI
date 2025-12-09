520061cbfb53fd4c49a96711d9614aac240d4e32
raz-th <raz.taz032023@gmail.com>
2025-12-09 22:34:18 +0200

    Incorporated OLED_Stats files

---
+++ OLED_Stats/message.py
@@ -0,0 +1,56 @@
+import time
+import board
+import busio
+import digitalio
+
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+import subprocess
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Display Parameters
+WIDTH = 128
+HEIGHT = 32
+BORDER = 5
+
+# Display Refresh
+LOOPTIME = 1.0
+
+# Use for I2C.
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+# Clear display.
+oled.fill(0)
+oled.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+font = ImageFont.truetype('PixelOperator.ttf', 20)
+#font = ImageFont.load_default()
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+
+    # Pi Stats Display
+    draw.text((0, 0), "buna ce faci", font=font, fill=255)
+        
+    # Display image
+    oled.image(image)
+    oled.show()
+    time.sleep(LOOPTIME)
